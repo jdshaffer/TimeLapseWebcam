c161f53596d65d679016af4c35be28fed581bf08
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2024-10-21 12:10:10 +0900

    Update convert_to_movie.py
    Updates the comments to reflect that this is the final version.

--- convert_to_movie.py
+++ convert_to_movie.py
@@ -1,12 +1,12 @@
-################################################################
-# Takes a series of JPG images and turns then into an MP4 video
+##################################################################################
+# Takes a series of JPG images and turns then into an MP4 video  (Final Version)
 # Jeffrey D. Shaffer
-# 2024-10-16
+# 2024-10-21
 #
-# Requires this python module:
+# Requires the opencv-python module:
 #    pip install opencv-python
 #
-################################################################
+##################################################################################
 
 import cv2
 import os
