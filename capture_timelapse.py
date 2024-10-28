757938ca7bdd406ffe32fd88d84a60c03aebb167
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2024-10-28 22:18:37 +0900

    Update capture_timelapse.py
    Want to be transparent in how it was written.

--- capture_timelapse.py
+++ capture_timelapse.py
@@ -1,6 +1,6 @@
 #################################################################################################
 # Take time-lapse pictures using the WebCam  (Final Version)
-# Jeffrey D. Shaffer
+# Code developed by Jeffrey D. Shaffer with assistance from Claude Sonnet
 # 2024-10-21
 #
 # Requires the opencv-python module:
